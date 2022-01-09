1388fb2b1ea1da03c5e93e6889df846af0194403
pcancio <pcancio@gmail.com>
2022-01-09 14:10:46 -0500

    connection.js edited

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: 'employee-tracker'
+    database: 'employee_tracker'
 });
 
 module.exports = db;
