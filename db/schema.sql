a32c0b255f62e7ba0aba42e627894a7744596ced
pcancio <pcancio@gmail.com>
2022-01-09 14:03:37 -0500

    mysql

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE department (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary  
+)
